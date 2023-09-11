910cb187eae4f99b1ead2628bfa43e5ae37381bd
oleglevchuk1999 <o.levghyk@gmail.com>
2023-09-11 14:14:52 +0300

    Text and Loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Header from './components/Header/Header';
+import HomeText from './components/HomeText/HomeText';
+import Loader from './components/Loader/Loader';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <HomeText/>
+      <Loader/>
     </div>
   );
 }
---
+++ src/components/HomeText/HomeText.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomeText.css';
+
+const HomeText = () => {
+    return ( 
+        <div class="wrapper">
+        <svg>
+            <text x="50%" y="50%" dy=".35em" text-anchor="middle">
+                YLEGSEY
+            </text>
+        </svg>
+    </div> 
+    );
+}
+ 
+export default HomeText;
